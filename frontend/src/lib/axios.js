cff9ddb863ea96317a6f9596feffbe7cb332c118
mominaimran <mominaimran957@gmail.com>
2025-09-06 12:37:54 +0500

    added vercel.json

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
+// axios.js
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
+  withCredentials: true,
 });
